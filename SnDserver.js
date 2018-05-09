e6d06407f5807f62d58e642fe7f102675a0878ee
Will Jackson <21249756+wjjackson7@users.noreply.github.com>
2018-05-09 14:29:41 -0700

    progress from tuesday

--- SnDserver.js
+++ SnDserver.js
@@ -2,6 +2,27 @@ var http  = require('http');
 var static = require('node-static');
  
 var fileServer = new static.Server('./public');
+
+// global variables
+var fs = require('fs');  // file access module
+var imgList = [];
+loadImageList();
+
+// just for testing, you can cut this out
+console.log(imgList.length);
+
+function loadImageList () {
+    var data = fs.readFileSync('photoList.json');
+    if (! data) {
+        console.log("cannot read photoList.json");
+    } else {
+        listObj = JSON.parse(data);
+        imgList = listObj.photoURLs;
+    }
+}
+
+
+
 function handler (request, response) {
 
     request.addListener('end', function () {
@@ -10,16 +31,32 @@ function handler (request, response) {
 			var url = request.url;
 		    url = url.replace("/","");
 
-		    if(url.substring(0, 5) == "query" ){ // valid query 
-            	response.writeHead(404, {"Content-Type": "text/html"});
-				response.write("<p>Query is <code>" + url.substring(5, url.length-1) + "</code></p>");				response.end();
+		    if(url.substring(0, 10) == "query?num=" ){ // valid query 
+
+                // query >989 || < 0
+                if(parseFloat(url.substring(10, url.length)) > 988 || parseFloat(url.substring(10, url.length)) < 0) {
+                    response.writeHead(404, {"Content-Type": "text/html"});
+                    response.write("<p>Bad Query</p>");
+                    console.log("bad query");
+                    response.end();
+                }
+                // valid query
+                else {
+                    response.writeHead(404, {"Content-Type": "text/html"});
+                    response.write("<p>Query is <code>" + url.substring(10, url.length) + "</code></p>");  
+
+                    response.write("<p>" + imgList[parseFloat(url.substring(10, url.length))] + "</p>");            
+                    console.log(url.substring(10, url.length));
+                    response.end();
+                }
             }
 
-            else if (e && (e.status === 404)) { // If the file wasn't found
+            else if(e && (e.status === 404)) { // If the file wasn't found
                     response.writeHead(404, {"Content-Type": "text/html"});
 				    response.write("<p>404 not found</p>");
 				    response.end();
             }
+
 
         });
     }).resume();
